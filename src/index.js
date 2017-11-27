2ff590e0505d3260bbba63a2eaf8a83a433ffebf
Karthik Jagadeesh <kartu8@gmail.com>
2017-11-27 18:08:29 +0530

    Draw fractal tree

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import p5 from "p5";
+
+const app = p => {
+  p.setup = _ => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(0);
+
+    p.translate(p.width / 2, p.height);
+    drawBranch(p.TWO_PI, -200);
+  };
+
+  p.draw = _ => {};
+
+  function drawBranch(degrees, offsetY) {   
+    p.rotate(degrees);
+    p.stroke(p.random(255), p.random(255), p.random(255));
+    p.strokeWeight(2);
+    p.line(0, 0, 0, offsetY);
+    p.translate(0, offsetY);
+    if (offsetY < -4) {
+      p.push();
+      drawBranch(p.PI / 6, offsetY * 0.67);
+      p.pop();
+      p.push();
+      drawBranch(p.PI / -6, offsetY * 0.67);
+      p.pop();
+    }
+  }
+};
+
+const appInstance = new p5(app);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: "dist"
+  },
+  // devtool: "eval-cheap-module-source-map" 
+};
+
+module.exports = config;
